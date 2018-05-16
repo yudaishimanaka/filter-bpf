1ec727821053f9bc6a01c1a7c7cb27c003f8016b
yudaishimanaka <gurutaminn.h8@gmail.com>
2018-05-16 17:39:30 +0900

    fixed all

---
+++ cbpf.h
@@ -0,0 +1,22 @@
+/*
+ * cBPF helpers, partially taken from netsniff-ng
+ *
+ * Copyright (C) 2017 Tobias Klauser
+ * Copyright (C) 2009 - 2012 Daniel Borkmann.
+ * Copyright (C) 2009, 2010 Emmanuel Roullit.
+ * Subject to the GPL, version 2.
+ */
+
+#ifndef __CBPF_H
+#define __CBPF_H
+
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdlib.h>
+
+#include <linux/filter.h>
+
+void cbpf_dump_all(struct sock_fprog *bpf, bool raw);
+int cbpf_validate(const struct sock_fprog *bpf);
+
+#endif /* __CBPF_H */
