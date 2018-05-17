ee371ec1bff3b855fb32f6006fb725fdc5e4924e
yudaishimanaka <gurutaminn.h8@gmail.com>
2018-05-17 23:09:00 +0900

    fixed header file

--- get_iftype.h
+++ get_iftype.h
@@ -5,6 +5,6 @@
 #ifndef FILTER_BPF_GET_IFTYPE_H
 #define FILTER_BPF_GET_IFTYPE_H
 
-int dev_get_iftype(const char *ifname)
+int dev_get_iftype(const char *ifname);
 
 #endif //FILTER_BPF_GET_IFTYPE_H
